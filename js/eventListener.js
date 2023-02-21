67d9caa93ebaf8367b307ea8c2a8baa13a4d8a8c
Subhrajyoti Mahato <86191223+Subhrajyoti2608@users.noreply.github.com>
2023-02-21 14:19:25 +0530

    Create eventListener.js

---
+++ js/eventListener.js
@@ -0,0 +1,45 @@
+AFRAME.registerComponent("marker-handler",{
+init: async function(){
+this.el.addEventListener("markerFound", ()=>{
+console.log("marker is found!")
+  this.handleMarkerFound()
+})
+  
+this.el.addEventListener("markerLost", ()=>{
+console.log("marker is lost!")
+  this.handleMarkerLost()
+})
+},
+  
+handleMarkerFound: function(){
+var buttonDiv = document.getElementById("button-div")
+
+buttonDiv.style.display = "flex"
+  
+var ratingButton = document.getElementById("rating-button")
+
+var orderButton = document.getElementById("order-button")
+
+ratingButton.addEventListener("click", function(){
+  swal({
+  icon:"warning",
+  title:"rate toy",
+  text:"work in progress"
+  })
+})
+  
+  orderButton.addEventListener("click", function(){
+  swal({
+  icon:"warning",
+  title:"Thanks for ordering!",
+  text:"It will be delivered soon!"
+  })
+})
+},
+  
+ handleMarkerLost: function(){
+ var buttonDiv = document.getElementById("button-div")
+
+ buttonDiv.style.display = "none";
+ }
+})
